cebd58182c715089a1e1b2b93b20668c321d5744
Jacob Chapel <jacob.chapel@gmail.com>
2024-11-30 20:58:18 -0800

    day01: dawn of the first day

--- day01/main.ts
+++ day01/main.ts
@@ -1,3 +1,40 @@
-export function day01() {
-  return "Day 01";
+function getLists(input: string) {
+  const reg = /(\d+)\s+(\d+)/g;
+  return input
+    .split("\n")
+    .map((line) => {
+      const [_, first, second] = line.matchAll(reg).next().value || [];
+      return [Number(first), Number(second)];
+    })
+    .reduce(
+      (lists, line) => {
+        lists.left.push(line[0]);
+        lists.right.push(line[1]);
+        return lists;
+      },
+      { left: [] as number[], right: [] as number[] }
+    );
+}
+
+export function part1(input: string): number {
+  const { left, right } = getLists(input);
+
+  left.sort();
+  right.sort();
+
+  let total = 0;
+  for (let i = 0; i < left.length; i += 1) {
+    total += Math.abs(left[i] - right[i]);
+  }
+
+  return total;
+}
+
+export function part2(input: string): number {
+  const { left, right } = getLists(input);
+  const total = left.reduce((total, lItem) => {
+    total += lItem * right.filter((rItem) => lItem === rItem).length;
+    return total;
+  }, 0);
+  return total;
 }
--- day01/main_test.ts
+++ day01/main_test.ts
@@ -1,6 +1,32 @@
 import { assertEquals } from "@std/assert";
-import { day01 } from "./main.ts";
+import { part1, part2 } from "./main.ts";
 
-Deno.test(function day01Test() {
-  assertEquals(day01(), "Day 01");
+const example = `
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3`.trim();
+
+const input = await Deno.readTextFile("./input.txt");
+
+Deno.test("Part 1", async (t) => {
+  await t.step("Example Input", () => {
+    assertEquals(part1(example), 11);
+  });
+
+  await t.step("Input", () => {
+    assertEquals(part1(input), 2166959);
+  });
+});
+
+Deno.test("Part 2", async (t) => {
+  await t.step("Example Input", () => {
+    assertEquals(part2(example), 31);
+  });
+
+  await t.step("Input", () => {
+    assertEquals(part2(input), 23741109);
+  });
 });
